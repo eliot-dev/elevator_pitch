db5e3693aae77afb903112d98fe5dc95f4e767cf
Eliot <eliotbarnes5412@gmail.com>
2023-08-21 20:47:25 -0600

    init commit

---
+++ elevator-pitch/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Elevator Pitch!</title>
+      </Head>
+      <main >
+        <div>
+          <p>
+            Hello World!
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
